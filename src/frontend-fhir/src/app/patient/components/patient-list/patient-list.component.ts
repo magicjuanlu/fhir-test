836648fb4a0721a5f91204608f4695247b4aef12
Juan Luis <juanluismgr25@hotmail.com>
2023-06-12 00:28:00 +0200

    Primeng added and select row navigation to detail

--- src/frontend-fhir/src/app/patient/components/patient-list/patient-list.component.ts
+++ src/frontend-fhir/src/app/patient/components/patient-list/patient-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PatientService } from '../../services/patient.service';
 import { IPatient } from '../../models/patient.model';
+import { TableRowSelectEvent } from 'primeng/table';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-patient-list',
@@ -10,12 +12,23 @@ import { IPatient } from '../../models/patient.model';
 export class PatientListComponent implements OnInit {
   patients: IPatient[] = [];
 
-  constructor(private patientService: PatientService) {}
+  constructor(
+    private patientService: PatientService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.patientService.getPatientList().subscribe((list) => {
       console.log(list);
       this.patients = list;
     });
+  }
+
+  onRowSelect(event: TableRowSelectEvent) {
+    const patient = event.data as IPatient;
+    if (patient.id) {
+      this.router.navigate([patient.id], { relativeTo: this.route });
+    }
   }
 }
--- src/frontend-fhir/src/app/patient/patient.module.ts
+++ src/frontend-fhir/src/app/patient/patient.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { PatientListComponent } from './components/patient-list/patient-list.component';
 import { PatientDetailsComponent } from './components/patient-details/patient-details.component';
 import { PatientRoutingModule } from './patient-routing.module';
+import { SkeletonModule } from 'primeng/skeleton';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [PatientListComponent, PatientDetailsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, SkeletonModule, TableModule],
   exports: [PatientRoutingModule],
 })
 export class PatientModule {}
